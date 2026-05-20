82d76eab983f2232fb1b32042245c60260812771
chanhongkwak <rhkrcksghd1@gmail.com>
2026-05-20 14:45:25 +0900

    FIX/product/AwsProperties값검증추가

--- src/main/java/com/ezmeal/product/infrastructure/config/AwsProperties.java
+++ src/main/java/com/ezmeal/product/infrastructure/config/AwsProperties.java
@@ -1,15 +1,20 @@
 package com.ezmeal.product.infrastructure.config;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.validation.annotation.Validated;
 
+@Validated
 @ConfigurationProperties(prefix = "cloud.aws")
 public record AwsProperties(
-        String region,
-        S3 s3
+        @NotBlank String region,
+        @NotNull @Valid S3 s3
 ) {
     public record S3(
-            String bucket,
-            String imageBaseUrl
+           @NotBlank String bucket,
+           @NotBlank String imageBaseUrl
     ) {
     }
 }
